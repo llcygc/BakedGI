7bc1924323e9797b843a916e5bad5798e2e85dfd
FelixWang <llcygc@126.com>
2018-10-10 00:37:27 +0800

    Light buffers

--- BakedGI/Source/Graphics/LightManager.h
+++ BakedGI/Source/Graphics/LightManager.h
@@ -1,8 +1,23 @@
 #pragma once
+
+#include "PipelineState.h"
+#include "GpuBuffer.h"
+
 class LightManager
 {
 public:
 	LightManager();
 	~LightManager();
+
+public:
+	ByteAddressBuffer m_StartOffsetIndexBuffer;
+	StructuredBuffer m_LightLinkListBuffer;
+
+	StructuredBuffer m_DirectionalLightsBuffer;
+	StructuredBuffer m_PunctualLightsGeoBuffer;
+	StructuredBuffer m_PunctualLightsRenderBuffer;
+
+	ComputePSO m_ClusterLightAssignment_32;
+
 };
 
