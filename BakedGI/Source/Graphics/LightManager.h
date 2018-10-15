4ec7ed8937b049a64ccf91188d7f9db15894e89a
llcygc <llcygc@126.com>
2018-10-15 21:00:56 +0800

    Add several functions

--- BakedGI/Source/Graphics/LightManager.h
+++ BakedGI/Source/Graphics/LightManager.h
@@ -2,12 +2,15 @@
 
 #include "PipelineState.h"
 #include "GpuBuffer.h"
+#include "ShadowManager.h"
 
 class LightManager
 {
 public:
 	LightManager();
 	~LightManager();
+
+	void PrepareLightsDataForGPU();
 
 public:
 	ByteAddressBuffer m_StartOffsetIndexBuffer;
@@ -19,5 +22,11 @@ public:
 
 	ComputePSO m_ClusterLightAssignment_32;
 
+private:
+	ShadowManager m_ShadowManager;
+
+	const int MAX_LOCAL_LIGHTS_COUNT = 20;
+	const int MAX_SHADOW_CASTER_COUNT = 10;
+	const int MAX_DIRECTIONAL_LIGHTS_COUNT = 4;
 };
 
